1fa79a6e853536d1e296450dc26fc28d8c09a5ea
Wilson Li <windkithk@gmail.com>
2015-04-14 16:28:58 +0900

    Added build_deps Script as a pre_hook

---
+++ c_src/build_deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+test `basename $PWD` != "c_src" && cd c_src
+
+cd ../deps/gfcomplete
+export GFP=`pwd`
+if [ ! -f src/.libs/libgf_complete.so ]; then
+    autoreconf --install
+    ./configure
+    make -j4
+fi
+cd ../jerasure
+if [ ! -f src/.libs/libJerasure.so ]; then
+    autoreconf --install
+    ./configure LDFLAGS=-L$GFP/src/.libs/ CPPFLAGS=-I$GFP/include
+    make -j4
+fi
+
+cd $PWD
