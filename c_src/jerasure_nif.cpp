02322a9908b907b1d7187f70de4b91bef9b1e70d
Wilson Li <windkithk@gmail.com>
2015-04-15 12:50:01 +0900

    Fixed C Source Code

--- c_src/jerasure_nif.cpp
+++ c_src/jerasure_nif.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <vector>
@@ -29,18 +30,19 @@ uint32_t roundTo(uint32_t numToRound, uint32_t multiple) {
 }
 
 vector<ErlNifBinary> doEncode(unsigned char* data, size_t dataSize, int k, int m, int w, CodingType coding) {
+    vector<ErlNifBinary> allBlockBinary;
     int *matrix = cauchy_good_general_coding_matrix(k, m, w);
     int *bitmatrix = jerasure_matrix_to_bitmatrix(k, m, w, matrix);
     int **smart = jerasure_smart_bitmatrix_to_schedule(k, m, w, bitmatrix);
 
-    long long blockSize = roundTo((roundTo(dataSize, k*w) / (k*w)), 4);
+    long long blockSize = roundTo((roundTo(dataSize, k*w) / (k*w)), 4) * w;
+
 
     char** dataBlocks = (char**)enif_alloc(sizeof(char*) * k);
-    vector<ErlNifBinary> allBlockBinary;
     for(int i = 0; i < k - 1; ++i) {
         ErlNifBinary dataBlock;
         enif_alloc_binary(blockSize, &dataBlock);
-        memcpy(dataBlock.data, data + blockSize * (k-1), blockSize);
+        memcpy(dataBlock.data, data + blockSize * i, blockSize);
         allBlockBinary.push_back(dataBlock);
         dataBlocks[i] = (char*)dataBlock.data;
     }
@@ -49,6 +51,8 @@ vector<ErlNifBinary> doEncode(unsigned char* data, size_t dataSize, int k, int m
     ErlNifBinary lastBlock;
     enif_alloc_binary(blockSize, &lastBlock);
     memset(lastBlock.data, 0, blockSize);
+    printf("Data Size: %d , Blocks Size: %d (%d,%d)\n", dataSize, blockSize, k, m);
+    printf("Copy Size: %d\n", dataSize - (blockSize * (k - 1)));
     memcpy(lastBlock.data, data + blockSize * (k - 1), dataSize - (blockSize * (k - 1)));
     allBlockBinary.push_back(lastBlock);
     dataBlocks[k - 1] = (char*)lastBlock.data;
@@ -61,7 +65,7 @@ vector<ErlNifBinary> doEncode(unsigned char* data, size_t dataSize, int k, int m
         codeBlocks[i] = (char*)codeBlock.data;
     }
 
-    jerasure_schedule_encode(k, m, w, smart, dataBlocks, codeBlocks, w*blockSize, blockSize);
+    jerasure_schedule_encode(k, m, w, smart, dataBlocks, codeBlocks, blockSize, blockSize / w);
 
     enif_free(dataBlocks);
     enif_free(codeBlocks);
@@ -85,10 +89,11 @@ encode_test(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     vector<ErlNifBinary> ret = doEncode(data, totalSize, k, m, 8, RS_CAUCHY);
     
     ERL_NIF_TERM retArr[k + m];
-    for(int i = 0; i < k + m; ++i) {
+//    for(int i = 0; i < k + m; ++i) {
+    for(int i = 0; i < ret.size(); ++i) {
         retArr[i] = enif_make_binary(env, &ret[i]);
     }
-    return enif_make_list_from_array(env, retArr, k + m);
+    return enif_make_list_from_array(env, retArr, ret.size());
 }
 
 static ErlNifFunc nif_funcs[] = {
