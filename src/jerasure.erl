9240b77ae6ba97251f14253dbe1fc66100de0097
Wilson Li <windkithk@gmail.com>
2015-04-15 12:50:16 +0900

    Completed Encode Test

--- src/jerasure.erl
+++ src/jerasure.erl
@@ -1,11 +1,12 @@
 -module(jerasure).
 
--export([encode_test/2, decode_test/2]).
+-export([encode_test/1,encode_test/2, decode_test/2]).
 
 -on_load(init/0).
 
 -define(APPNAME, jerasure).
 -define(LIBNAME, jerasure_drv).
+-define(BLOCKSTOR, "blocks/").
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -23,10 +24,34 @@ init() ->
         Dir ->
             filename:join(Dir, ?LIBNAME)
     end,
+    filelib:ensure_dir(?BLOCKSTOR),
     erlang:load_nif(SoName, 0).
 
+write_blocks(_, [], Cnt) ->
+    Cnt;
+write_blocks(FileName, [H | T], Cnt) ->
+    BlockName = FileName ++ "." ++ integer_to_list(Cnt),
+    BlockPath = filename:join(?BLOCKSTOR, BlockName),
+    file:write_file(BlockPath, H),
+    write_blocks(FileName, T, Cnt + 1).
+
+encode(FileName) ->
+    case file:read_file(FileName) of
+        {ok, FileContent} ->
+            io:format("File Content Length: ~p~n", [byte_size(FileContent)]),
+            Blocks = encode_test(FileContent, byte_size(FileContent)),
+            io:format("Number of Blocks: ~p~n", [length(Blocks)]);
+        {error, Reason} ->
+            Blocks = [],
+            erlang:error(Reason)
+    end,
+    write_blocks(FileName, Blocks, 0).
+
+decode(FileName, FileSize) ->
+    
+
 encode_test(_,_) ->
-    exit(nif_library_no_loaded).
+    exit(nif_library_not_loaded).
 
 decode_test(_,_) ->
-    exit(nif_library_no_loaded).
+    exit(nif_library_not_loaded).
