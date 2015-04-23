113046446ed7f59e547c4403dbfbd7d42fbd939c
Wilson Li <windkithk@gmail.com>
2015-04-23 16:07:19 +0900

    [Fix] Fixed for Cauchy RS

--- c_src/cauchycoding.h
+++ c_src/cauchycoding.h
@@ -1,11 +1,11 @@
-#ifndef __RS_CODING_H__
-#define __RS_CODING_H__
+#ifndef __CAUCHY_CODING_H__
+#define __CAUCHY_CODING_H__
 
 #include "coding.h"
 
-class RSCoding : public Coding {
+class CAUCHYCoding : public Coding {
     public:
-        RSCoding(int k, int m, int w) : Coding(k, m, w) {};
+        CAUCHYCoding(int k, int m, int w) : Coding(k, m, w) {};
         vector<ErlNifBinary> doEncode(unsigned char* data, size_t dataSize);
         ErlNifBinary doDecode(vector<ErlNifBinary> blockList, vector<int> blockIdList, size_t dataSize);
 
