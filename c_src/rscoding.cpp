9323747d337c56548f85c9475464a448f4fc9f00
Wilson Li <windkithk@gmail.com>
2015-04-27 16:54:33 +0900

    [VandRS] About Issue #4, changed to pre-allocate large memory piece and added check

--- c_src/rscoding.cpp
+++ c_src/rscoding.cpp
@@ -19,24 +19,45 @@ vector<ErlNifBinary> RSCoding::doEncode(unsigned char* data, size_t dataSize) {
 
     int *matrix = reed_sol_vandermonde_coding_matrix(k, m, w);
 
-    size_t blockSize = roundTo((roundTo(dataSize, k*w) / (k*w)), sizeof(long)) * w;
+    size_t blockSize = roundTo((roundTo(dataSize, k*w) / (k*w)), 16) * w;
 
     /// TODO: Hack to make memory allocation 16-byte aligned, force min block size to 512KB
-    if (blockSize < 512 << 10)
-        blockSize = 512 << 10;
+//    if (blockSize < 512 << 10)
+//        blockSize = 512 << 10;
 
     char** dataBlocks = (char**)alloc(sizeof(char*) * k);
     char** codeBlocks = (char**)alloc(sizeof(char*) * m);
+
+    unsigned int align;
+    bool aligned = true;
+    size_t offset = 0;
 
     for(int i = 0; i < k; ++i) {
         ErlNifBinary tmpBlock;
 
         enif_alloc_binary(blockSize, &tmpBlock);
+
+        // Alignment with respect to each other along 16-byte Boundary
+        if (i == 0)
+            align = (unsigned long)tmpBlock.data & 0x0f;
+        else if (((unsigned long)tmpBlock.data & 0x0f) != align){
+            aligned = false;
+        }
+
         dataBlocks[i] = (char*)tmpBlock.data;
 
         allBlockEntry.push_back(tmpBlock);
         
-        memset(dataBlocks[i], 0, blockSize);
+        // Setup Data Blocks
+        if (offset < dataSize) {
+            size_t copySize = min(dataSize - offset, blockSize);
+            if (copySize < blockSize)
+                memset(dataBlocks[i], 0, blockSize);
+            memcpy(dataBlocks[i], data + offset, copySize);
+            offset += copySize;
+        } else {
+            memset(dataBlocks[i], 0, blockSize);        
+        }
         /*
         if (i == k - 1) {
             memset(dataBlocks[i], 0, blockSize);
@@ -47,27 +68,40 @@ vector<ErlNifBinary> RSCoding::doEncode(unsigned char* data, size_t dataSize) {
         offset += blockSize;
         */
     }
-
-    size_t offset = 0;
-    int i = 0;
-    while(offset < dataSize) {
-        size_t copySize = min(dataSize - offset, blockSize);
-        memcpy(dataBlocks[i], data + offset, copySize);
-        i++;
-        offset += copySize;
-    }
-
+    
     for(int i = 0; i < m; ++i) {
         ErlNifBinary tmpBlock;
 
         enif_alloc_binary(blockSize, &tmpBlock);
+        if (((unsigned long)tmpBlock.data & 0x0f) != align){
+            aligned = false;
+        }
         codeBlocks[i] = (char*)tmpBlock.data;
 
         allBlockEntry.push_back(tmpBlock);
     }
 
-    jerasure_matrix_encode(k, m, w, matrix, dataBlocks, codeBlocks, blockSize);
+    char* tmpMemory = NULL;
+    // Encode in Pre-allocated Space Instead
+    if (!aligned) {
+        tmpMemory = (char*)alloc(blockSize * (k + m));
+        memset(tmpMemory, 0, blockSize * (k + m));
+        memcpy(tmpMemory, data, dataSize);
+        for(int i = 0; i < k + m; ++i) {
+            (i < k) ? dataBlocks[i] = tmpMemory + i * blockSize :
+                codeBlocks[i - k] = tmpMemory + i * blockSize;
+        }
+    }
 
+    jerasure_matrix_encode(k, m, w, matrix, dataBlocks, codeBlocks, blockSize);
+    
+    // Copy Back the Code Blocks
+    if (!aligned) {
+        for(int i = 0; i < m; ++i) {
+            memcpy(allBlockEntry[i + k].data, codeBlocks, blockSize);
+        }
+        dealloc(tmpMemory);
+    }
     free(matrix);
     dealloc(dataBlocks);
     dealloc(codeBlocks);
