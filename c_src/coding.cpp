99ab247817e63e6ea9d694318c7b83f0562aefb1
Wilson Li <windkithk@gmail.com>
2015-05-15 09:46:49 +0900

    [Interface] Fixed Memory Leak in Block Repair

--- c_src/coding.cpp
+++ c_src/coding.cpp
@@ -6,7 +6,7 @@ ErlNifBinary Coding::doRepair(vector<ErlNifBinary> blockList, vector<int> blockI
     enif_release_binary(&dec);
     for(size_t i = 0; i < enc.size(); ++i) {
         if (i != (size_t)repairId)
-            enif_release_binary(&dec);
+            enif_release_binary(&enc[i]);
     }
     return enc[repairId];
 }
