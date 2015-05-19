c3d85a817cac7976b8ee611095305732f09d6ef8
Wilson Li <windkithk@gmail.com>
2015-05-19 16:07:29 +0900

    [Backend] Removed interface of repair_one

--- c_src/coding.cpp
+++ c_src/coding.cpp
@@ -1,13 +1,5 @@
 #include "coding.h"
 
-ERL_NIF_TERM Coding::doRepair(vector<ERL_NIF_TERM> blockList, vector<int> blockIdList, int repairId) {
-    ErlNifBinary bin;
-    enif_inspect_binary(env, blockList[0], &bin);
-    ERL_NIF_TERM file = doDecode(blockList, blockIdList, k * bin.size);
-    vector<ERL_NIF_TERM> blocks = doEncode(file); 
-    return blocks[repairId];
-}
-
 vector<ERL_NIF_TERM> Coding::doRepair(vector<ERL_NIF_TERM> blockList, vector<int> blockIdList, vector<int> repairList) {
     ErlNifBinary bin;
     enif_inspect_binary(env, blockList[0], &bin);
